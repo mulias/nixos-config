2a0ca888a6f0eab5246b9079b227820b11575145
Elias Mulhall <eli.mulhall@gmail.com>
2024-11-24 12:13:56 -0500

    update

--- configuration.nix
+++ configuration.nix
@@ -3,156 +3,185 @@
 {
   imports = [ ./hardware-configuration.nix ];
 
+  system = {
+    stateVersion = "24.05";
+
+    autoUpgrade = {
+      enable = true;
+      channel = https://nixos.org/channels/nixos-24.05;
+    };
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   boot = {
+    kernelPackages = pkgs.linuxKernel.packages.linux_5_15;
+
+    blacklistedKernelModules = [ "bluetooth" "btusb" ];
+
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
       grub.configurationLimit = 15;
     };
-
-    # Thinkpad battery module for TLP
-    kernelModules = [ "acpi_call" ];
-    extraModulePackages = with config.boot.kernelPackages; [ acpi_call ];
   };
 
-  services.xserver = {
-    enable = true;
-    libinput.enable = true;
-
-    displayManager.defaultSession = "xfce+i3";
-
-    # Simple login screen
-    displayManager.lightdm.greeters.mini = {
+  services = {
+    xserver = {
       enable = true;
-      user = "elias";
-      extraConfig = ''
-        [greeter]
-        show-password-label = false
-        password-alignment = left
-        [greeter-theme]
-        text-color = "#080800"
-        error-color = "#F8F8F0"
-        background-image = ""
-        background-color = "#1B1D1E"
-        window-color = "#1B1D1E"
-        border-color = "#080800"
-        border-width = 0px
-        layout-space = 0
-        password-color = "#F8F8F0"
-        password-background-color = "#1B1D1E"
-      '';
-    };
 
-    # Xfce manages themes, default applications, mounting media, etc. When
-    # first configuring a new system you'll want to run `xfce4-settings-manager`
-    # to make stateful changes such as setting the window and icon styles.
-    desktopManager = {
-      xterm.enable = false;
-      xfce = {
-        enable = true;
-        noDesktop = true;
-        enableXfwm = false;
+      displayManager.startx.enable = true;
+
+      windowManager.i3.enable = true;
+
+      # Xfce manages themes, default applications, mounting media, etc. When
+      # first configuring a new system you'll want to run `xfce4-settings-manager`
+      # to make stateful changes such as setting the window and icon styles.
+      desktopManager = {
+        xterm.enable = false;
+        xfce = {
+          enable = true;
+          noDesktop = true;
+          enableXfwm = false;
+        };
       };
     };
 
-    # Use i3 with local config
-    windowManager = {
-      i3.enable = true;
+    displayManager.defaultSession = "xfce+i3";
+
+    libinput.enable = true;
+
+    connman.enable = true;
+
+    printing.enable = false;
+
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
     };
   };
 
-  # Networking with connman
-  networking.wireless = {
-    enable = true;
-    interfaces = [ "wlp0s20f3" ];
-    networks = {
-      # Fake ssid so NixOS creates wpa_supplicant.conf
-      # otherwise the service fails and WiFi is not available.
-      # https://github.com/NixOS/nixpkgs/issues/23196
-      S4AKR00UNUN21W1NV2Y5MDDW8 = {};
+  networking = {
+    wireless = {
+      enable = true;
+      interfaces = [ "wlp0s20f3" ];
     };
-  };
-  services.connman.enable = true;
-  networking.firewall = {
-    enable = true;
-    # Allow ports needed for chromecast. See
-    # https://github.com/NixOS/nixpkgs/issues/49630
-    # Also open TCP ports for local development
-    allowedUDPPortRanges = [ { from = 32768; to = 60999; } ];
-    allowedTCPPorts = [ 8010 3000 4000 5432 ];
-  };
 
-  # Power management
-  services.tlp.enable = true;
-  systemd.services = {
-    battery_threshold = {
-      description = "Set battery charging thresholds.";
-      path = [ pkgs.tpacpi-bat ];
-      after = [ "basic.target" ];
-      wantedBy = [ "multi-user.target" ];
-      script = ''
-        tpacpi-bat -s start 0 70
-        tpacpi-bat -s stop 0 85
-      '';
+    firewall = {
+      enable = true;
+      # Allow ports needed for chromecast. See
+      # https://github.com/NixOS/nixpkgs/issues/49630
+      # Also open TCP ports for local development
+      allowedUDPPortRanges = [ { from = 32768; to = 60999; } ];
+      allowedTCPPorts = [ 8010 3000 4000 5432 55555 4567 ];
     };
   };
 
-  services.avahi.enable = true;
+  programs = {
+    dconf.enable = true;
+
+    gnupg.agent = {
+       enable = true;
+       enableSSHSupport = true;
+    };
+
+    thunar = {
+      enable = true;
+      plugins = with pkgs.xfce; [
+        thunar-archive-plugin
+        thunar-media-tags-plugin
+        thunar-volman
+      ];
+    };
+
+    zsh.enable = true;
+  };
 
   nixpkgs.config.allowUnfree = true;
+
   environment.systemPackages = with pkgs; [
+    (hunspellWithDicts [ hunspellDicts.en-us ])
+    (polybar.override { i3Support = true; pulseSupport = true; })
     acpi
+    adapta-gtk-theme
+    alsa-utils
     arandr
+    arc-icon-theme
+    baobab
+    bat
+    brightnessctl
+    catdocx
     cloc
-    coreutils
+    connman-gtk
     connman-ncurses
+    coreutils
+    direnv
+    diskus
+    emacs
+    fd
     feh
+    ffmpeg
+    figlet
     file
     firefox
+    #fzf
     git
+    gnome.file-roller
+    gnupg
     htop
-    (hunspellWithDicts [ hunspellDicts.en-us ])
+    hyperfine
+    jless
+    lighttpd
+    links2
+    mplayer
+    # neovim
     ntfs3g
+    pandoc
+    pavucontrol
+    pipewire
+    playerctl
+    pulseaudio
+    ripgrep
     scrot
+    silver-searcher
     stow
     tpacpi-bat
     tree
+    universal-ctags
+    unzip
     vim_configurable
+    vlc
     wget
     xarchiver
     xcape
     xclip
     xdotool
+    xfce.xfce4-volumed-pulse
+    xorg.xinit
     xorg.xmodmap
+    zoom-us
     zsh
   ];
 
-  fonts.fonts = with pkgs; [
-    inconsolata
-    font-awesome_5
-  ];
+  fonts.packages = with pkgs; [ inconsolata font-awesome_5 ];
 
   time.timeZone = "America/New_York";
 
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
-  programs.zsh.enable = true;
+  hardware.bluetooth.enable = false;
 
   users = {
     users.elias = {
       isNormalUser = true;
-      extraGroups = [ "wheel" "networkmanager" ];
+      extraGroups = [ "wheel" "networkmanager" "video" "docker" ];
     };
     defaultUserShell = pkgs.zsh;
   };
-
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "21.11";
-  # Automatically check for updates within 21.11
-  system.autoUpgrade.enable = true;
-  system.autoUpgrade.channel = https://nixos.org/channels/nixos-21.11;
 }
